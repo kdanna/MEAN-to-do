e8ba66f53aa57fc283711690e533d64cfec702c8
kdanna <kayce.danna@gmail.com>
2016-08-21 13:54:00 -0600

    mongodb,mongoose are set up and connected to api

---
+++ db/models/todo.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var todoSchema = new mongoose.Schema({
+	name: String,
+	completed: Boolean
+});
+
+var model = mongoose.model('Todo', todoSchema);
+
+module.exports = model;
